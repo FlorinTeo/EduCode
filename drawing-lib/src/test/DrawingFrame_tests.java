1511bec26215e3b9059cc4b93a78e3f6372d298b
Florin.Teo <Florin.Teo@live.com>
2025-02-04 10:09:38 -0800

    Incorporate unit tests in drawing-lib

---
+++ drawing-lib/src/test/DrawingFrame_tests.java
@@ -0,0 +1,53 @@
+import java.awt.image.BufferedImage;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+import edu.ftdev.Drawing;
+import edu.ftdev.DrawingFrame;
+import edu.ftdev.MouseInterceptor.MouseHook;
+
+public class DrawingFrame_tests {
+
+    public static MouseHook _onMouseClick = (MouseEvent e) -> {
+        int x = e.getX();
+        int y = e.getY();
+        System.out.printf("Clicked at (%d, %d)\n", x, y);
+    };
+
+    @Test
+    public void basicTest() throws IOException, InterruptedException {
+        System.out.println("DrawingLib testing code!");
+        File drwFile = new File("src/test/test_img1.jpg");
+        BufferedImage drwImg = ImageIO.read(drwFile);
+        Drawing drw = new Drawing(drwImg);
+        DrawingFrame drwFrame = new DrawingFrame(drw);
+        drwFrame.open();
+        System.out.println("Pausing");
+        drwFrame.step();
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+            drwFrame.step(1000);
+        }
+        System.out.println("Stopping");
+
+        // enabling the mouse hook. While mouse hook enabled, stop() and step() methods are inactive (pass-through)!
+        drwFrame.setMouseClickedHook(_onMouseClick);
+        drwFrame.stop();
+        System.out.println("MouseHook enabled for 10sec!");
+        Thread.sleep(10000);
+
+        // disable the mouse hook. When disabled, stop() and step() are active!
+        drwFrame.setMouseClickedHook(null);
+        System.out.println("MouseHook disabled!");
+        drwFrame.stop();
+
+        // close the frame
+        drwFrame.close();
+        System.out.println("Goodbye!");
+    }
+}
