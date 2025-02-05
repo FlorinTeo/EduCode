cdc595b1bc89a1de932926119892897fc65e11e8
Florin.Teo <Florin.Teo@live.com>
2025-02-04 21:52:05 -0800

    Restructure the github pages for GraphVisualizer

--- .github/gv-build.sh
+++ .github/gv-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 #
-# Simple build script that just copies the app into the build/ directory.
+# Build script that creates a build artifact for the GraphVisualizer web app
 #
 
 rm -rf .gv-build
+
 mkdir .gv-build/
-cp -r GraphVisualizer/*.ico GraphVisualizer/*.html GraphVisualizer/*.css GraphVisualizer/js GraphVisualizer/res GraphVisualizer/data .gv-build/
+cp index.html index.css favicon.ico .gv-build/
+cp -r GraphVisualizer/ .gv-build/
