fc4e9b6721feec44fc03bb47b3c82d4d9ee70cc7
Florin.Teo <Florin.Teo@live.com>
2025-02-04 20:57:50 -0800

    restructuring

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# Simple build script that just copies the app into the build/ directory.
-#
-
-rm -rf build
-mkdir build/
-cp -r GraphVisualizer/*.ico GraphVisualizer/*.html GraphVisualizer/*.css GraphVisualizer/js GraphVisualizer/res GraphVisualizer/data build/
