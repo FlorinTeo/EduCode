42e0288a9cd7e47437d179dba1e93bbaa3622cda
Florin.Teo <Florin.Teo@live.com>
2025-02-04 20:27:20 -0800

    correct gv-build.sh

--- .github/gv-build.sh
+++ .github/gv-build.sh
@@ -4,6 +4,6 @@
 # Simple build script that just copies the app into the build/ directory.
 #
 
-rm -rf build
-mkdir build/
-cp -r GraphVisualizer/*.ico GraphVisualizer/*.html GraphVisualizer/*.css GraphVisualizer/js GraphVisualizer/res GraphVisualizer/data build/
+rm -rf .gv-build
+mkdir .gv-build/
+cp -r GraphVisualizer/*.ico GraphVisualizer/*.html GraphVisualizer/*.css GraphVisualizer/js GraphVisualizer/res GraphVisualizer/data .gv-build/
