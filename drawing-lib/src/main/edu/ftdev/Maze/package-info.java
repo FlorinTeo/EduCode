35180e4f0992c1ef84363a94db8de7e6dda38922
Florin.Teo <Florin.Teo@live.com>
2025-02-26 12:58:46 -0800

    Correct @see reference

--- drawing-lib/src/main/edu/ftdev/Maze/package-info.java
+++ drawing-lib/src/main/edu/ftdev/Maze/package-info.java
@@ -3,8 +3,8 @@
  * and the building blocks for generating and displaying mazes. As an extension of
  * <i>DrawingFactory</i>, <i>MazeCanvas</i> provides simple ways to interact with the
  * maze window and to control the program execution.
- * @see DrawingFactory
- * @see FrameControls
- * @see DbgControls
+ * @see edu.ftdev.DrawingFactory
+ * @see edu.ftdev.FrameControls
+ * @see edu.ftdev.DbgControls
  */
 package edu.ftdev.Maze;
