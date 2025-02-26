1dfc32a08b5bc6425ed3ad814c57d66e8199bd67
Florin.Teo <Florin.Teo@live.com>
2025-02-25 20:24:53 -0800

    Fix javadocs

--- drawing-lib/src/main/edu/ftdev/DrawingFactory.java
+++ drawing-lib/src/main/edu/ftdev/DrawingFactory.java
@@ -21,6 +21,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
     /**
      * Opens a window on the screen, displaying the associated Drawing
      * and the controls for interacting with it. 
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      */
     @Override
     public void open() {
@@ -33,6 +34,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
     /**
      * Forces a refresh of the window content such that any changes that may have been
      * operated on the associated Drawing are reflected on the screen.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      */
     @Override
     public void repaint() {
@@ -46,6 +48,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
      * Prints out the given message in the status bar area, the lower right corner of
      * the drawing window.
      * @param message message to be printed in the status bar area.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      */
     @Override
     public void setStatusMessage(String message) {
@@ -57,6 +60,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
 
     /**
      * Closes the window.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      */
     @Override
     public void close() {
@@ -70,6 +74,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
     /**
      * Resets the Drawing back to its original state, when it was creating, removing any subsequent overlays that
      * may have been added to it.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @see Drawing#reset()
      */
     public void clear() {
@@ -82,6 +87,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
 
     /**
      * Gets the width of the drawing area.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @return the width of the drawing area in pixels.
      */
     public int getWidth() {
@@ -94,6 +100,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
     /**
      * Gets the height of the drawing area.
      * @return the heighth of the drawing area.
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      */
     public int getHeight() {
         if (_drawing == null) {
@@ -108,7 +115,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
      * When running in <i>step</i> mode, this method suspends the execution waiting for an explicit action to continue.
      * It does nothing in any other modes.
      * To resume, press any of the '1', '2', '3' or '&lt;space&gt;' keys or click on the the corresponding button on the DrawingFrame.
-     * @throws InterruptedException
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @see DbgControls#breakStep()
      */
     @Override
@@ -124,7 +131,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
      * It does nothing in any other mode.
      * To resume, press any of the '1', '2', '3' or '&lt;space&gt;' keys or click on the the corresponding button on the DrawingFrame.
      * @param delay milliseconds to delay the execution.
-     * @throws InterruptedException
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @see DbgControls#breakStep(long)
      */
     @Override
@@ -139,7 +146,7 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
      * When running in <i>step</i> or <i>leap</i> modes, this method pauses the execution waiting for an explicit action to continue.
      * It does nothing in <i>jump</i> or <i>run</i> modes. 
      * To resume, press any of the '1', '2', '3' or '&lt;space&gt;' keys or click on the the corresponding button on the DrawingFrame.
-     * @throws InterruptedException
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @see DbgControls#breakLeap()
      */
     @Override
@@ -152,9 +159,9 @@ public abstract class DrawingFactory implements DbgControls, FrameControls {
 
      /**
      * When running in <i>step</i>, <i>leap</i> or <i>jump</i> modes, this method pauses the execution waiting for an explicit action to continue.
-     * It does nothing in <i>>run</i> mode.
+     * It does nothing in <i>run</i> mode.
      * To resume, press any of the '1', '2', '3' or '&lt;space&gt;' keys or click on the the corresponding button on the DrawingFrame.
-     * @throws InterruptedException
+     * @throws IllegalStateException if the DrawingFrame has not been initialized.
      * @see DbgControls#breakJump()
      */
     @Override
