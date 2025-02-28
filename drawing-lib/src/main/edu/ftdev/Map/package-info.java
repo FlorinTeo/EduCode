a0986d19884717d3e6ee7f752ecbe2ffbe248858
Florin.Teo <Florin.Teo@live.com>
2025-02-28 10:49:48 -0800

    Javadocs adjustments, prep for map_canvas javadocs.

---
+++ drawing-lib/src/main/edu/ftdev/Map/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the <i>MapCanvas</i> class, which provides the abstractions 
+ * and the user interface for displaying and interacting with a street intersection on a road map.
+ * maze window and to control the program execution.
+ * @see edu.ftdev.DrawingFactory
+ * @see edu.ftdev.FrameControls
+ * @see edu.ftdev.DbgControls
+ */
+package edu.ftdev.Map;
