fa5f6c3f1e494c96d9880c09d8cc6a21d71f04ef
Florin.Teo <Florin.Teo@live.com>
2025-03-02 22:04:52 -0800

    Add package-info for SudokuBoard.

---
+++ drawing-lib/src/main/edu/ftdev/Sudoku/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains the <i>SudokuBoard</i> class, which provides the abstractions 
+ * and the building blocks for displaying and solving Sudoku board puzzles. As an extension of
+ * <i>DrawingFactory</i>, <i>SudokuBoard</i> provides simple ways to interact with the
+ * board window and to control the program execution.
+ * @see edu.ftdev.DrawingFactory
+ * @see edu.ftdev.FrameControls
+ * @see edu.ftdev.DbgControls
+ */
+package edu.ftdev.Sudoku;
