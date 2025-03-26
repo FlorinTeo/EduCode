7d6d7af4c9b2d45867d23ac8333d1a4ae861eddb
Florin.Teo <Florin.Teo@live.com>
2025-03-25 17:05:42 -0700

    Another trial

--- drawing-lib/src/test/STLModel_tests.java
+++ drawing-lib/src/test/STLModel_tests.java
@@ -5,7 +5,7 @@ import javafx.geometry.Point3D;
 
 public class STLModel_tests {
     @Test
-    public void basicTEst() {
+    public void basicTest() {
         STLPrism prism = new STLPrism(new Point3D(50, 50, 0), 20, 30, 10);
         System.out.println(prism.serialize());
     }
