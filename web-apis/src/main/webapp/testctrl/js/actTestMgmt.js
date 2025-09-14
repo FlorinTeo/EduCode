11643648e16b0ccb2cda976578cf0feb577f8f5f
Florin.Teo <Florin.Teo@live.com>
2025-09-14 12:07:32 -0700

    Comment out actTestMgmt

--- web-apis/src/main/webapp/testctrl/js/actTestMgmt.js
+++ web-apis/src/main/webapp/testctrl/js/actTestMgmt.js
@@ -7,8 +7,6 @@ let refAddLog;
 
 const actTestMgmt_edtTestName = document.getElementById("actTestMgmt_edtTestName");
 const actTestMgmt_edtFilter = document.getElementById("actTestMgmt_edtFilter");
-// const actTestMgmt_dlstTests = document.getElementById("actTestMgmt_dlstTests");
-// const actTestMgmt_dlstFilters = document.getElementById("actTestMgmt_dlstFilters");
 const actTestMgmt_ckbMCQ = document.getElementById("actTestMgmt_ckb_allMCQ");
 const actTestMgmt_lstMCQ = new CheckedList("actTestsMgmt_lstMCQ");
 const actTestMgmt_ckbFRQ = document.getElementById("actTestMgmt_ckb_allFRQ");
@@ -25,28 +23,30 @@ const actTestMgmt_questions = {
 };
 let actTestMgmt_qSelected = undefined;
 
+actTestMgmt_tglSolution.addEventListener("change", actTestMgmt_onToggleSolution); 
 actTestMgmt_edtFilter.addEventListener("input", actTestMgmt_onFilterChange);
-
 actTestMgmt_ckbMCQ.addEventListener("change", actTestMgmt_onCheckAll);
 actTestMgmt_lstMCQ.setEventListener("check", actTestMgmt_onCheckQuestion);
 actTestMgmt_lstMCQ.setEventListener("select", actTestMgmt_onSelectQuestion);
-
 actTestMgmt_ckbFRQ.addEventListener("change", actTestMgmt_onCheckAll);
 actTestMgmt_lstFRQ.setEventListener("check", actTestMgmt_onCheckQuestion);
 actTestMgmt_lstFRQ.setEventListener("select", actTestMgmt_onSelectQuestion);
-
 actTestMgmt_ckbAPX.addEventListener("change", actTestMgmt_onCheckAll);
 actTestMgmt_lstAPX.setEventListener("check", actTestMgmt_onCheckQuestion);
 actTestMgmt_lstAPX.setEventListener("select", actTestMgmt_onSelectQuestion);
 
-actTestMgmt_tglSolution.addEventListener("change", actTestMgmt_onToggleSolution); 
-
 // #region: exported methods
+/**
+ * Called from adminPanel, upon creating the action div element (once).
+ */
 export async function onCreate(sid, username, urlAPI, addLog) {
    refUrlAPI = urlAPI;
    refAddLog = addLog;
 }
 
+/**
+ * Called from adminPanel each time the action div becomes visible.
+ */
 export async function onOpen() {
    actTestMgmt_edtFilter.value = "";
    actTestMgmt_ckbMCQ.checked = false;
@@ -65,6 +65,9 @@ export async function onOpen() {
    request.send();
 }
 
+/**
+ * Called from the adminPanel when the "ack" (green) button is clicked on the dialog's title bar.
+ */
 export async function onApply() {
    if (actTestMgmt_edtTestName.value === "") {
       alert("Please provide test name!");
@@ -77,11 +80,36 @@ export async function onApply() {
    return true;
 }
 
+/**
+ * Called from the adminPanel when the "cancel" (red) button is clicked on the dialog's title bar.
+ */
 export async function onCancel() {
    refAddLog("actTestMgmt_onCancel called");
 }
 // #endregion: exported methods
 
+
+/**
+ * Extracts the list of query records (mapped to the jsonResponse) of a given type
+ * from the backend json response.
+ */
+function loadQSet(qTypes, jsonResponse) {
+   const lstTypes = qTypes.split("|");
+   let lstQRec = [];
+   for (const question of jsonResponse._qList) {
+      if (!lstTypes.includes(question._qType)) {
+         continue;
+      }
+      lstQRec.push(question);
+   }
+   return lstQRec;
+}
+
+/**
+ * Backend response handler for `${refUrlAPI}?cmd=query&type=qset`, loading the 
+ * list of all available questions (mcq, mcb, frq, apx) into the global container 
+ * of the three lists: _mcqRecs, _frqRecs, _apxRecs. Called from onOpen().
+ */
 function onQueryQSetResponse() {
    const jsonResponse = JSON.parse(this.response);
    if (this.status == 200) {
@@ -96,18 +124,9 @@ function onQueryQSetResponse() {
    }
 }
 
-function loadQSet(qTypes, jsonResponse) {
-   const lstTypes = qTypes.split("|");
-   let lstQRec = [];
-   for (const question of jsonResponse._qList) {
-      if (!lstTypes.includes(question._qType)) {
-         continue;
-      }
-      lstQRec.push(question);
-   }
-   return lstQRec;
-}
-
+/**
+ * Fills the lists in the action's div with the data from the question records.
+ */
 function initializeLists() {
    actTestMgmt_lstMCQ.clear();
    actTestMgmt_questions._mcqRecs.forEach(qRec => { actTestMgmt_lstMCQ.addItem(qRec._qName, qRec); })
@@ -117,12 +136,18 @@ function initializeLists() {
    actTestMgmt_questions._apxRecs.forEach(qRec => { actTestMgmt_lstAPX.addItem(qRec._qName, qRec); })
 }
 
+/**
+ * Handler called each time the user types a character in the filter input element.
+ */
 function actTestMgmt_onFilterChange(event) {
    actTestMgmt_lstFRQ.filter(actTestMgmt_edtFilter.value);
    actTestMgmt_lstMCQ.filter(actTestMgmt_edtFilter.value);
    actTestMgmt_lstAPX.filter(actTestMgmt_edtFilter.value);
 }
 
+/**
+ * Handler called when the user checks/unchecks any of the checkbox for the _mcq, _frq and _apx lists.
+ */
 function actTestMgmt_onCheckAll(event) {
    if (event.target === actTestMgmt_ckbMCQ) {
       actTestMgmt_lstMCQ.check(event.target.checked);
@@ -133,11 +158,17 @@ function actTestMgmt_onCheckAll(event) {
    }
 }
 
+/**
+ * Handler called each time a question checkbox is checked/unchecked.
+ */
 async function actTestMgmt_onCheckQuestion(event) {
    let question = event.metadata;
    question.checked = event.checked;
 }
 
+/**
+ * Handler called each time a question is selected/unselected
+ */
 async function actTestMgmt_onSelectQuestion(event) {
    if (event.target && event.selected) {
       if (event.host === actTestMgmt_lstMCQ) {
@@ -158,12 +189,18 @@ async function actTestMgmt_onSelectQuestion(event) {
    }
 }
 
+/**
+ * Handler called each time the "Solution" toggle is toggled.
+ */
 async function actTestMgmt_onToggleSolution(event) {
    if (actTestMgmt_qSelected) {
       actTestMgmt_divQuerySend(actTestMgmt_qSelected._qName, actTestMgmt_tglSolution.checked);
    }
 }
 
+/**
+ * Backend API request for fetching the content div for a specific query.
+ */
 function actTestMgmt_divQuerySend(qName, isAnswer) {
    const urlAPI_query = `${refUrlAPI}?cmd=query&type=${isAnswer ? "qanswer" : "qtest"}&qid=${qName}`;
    var request = new  XMLHttpRequest();
@@ -174,6 +211,10 @@ function actTestMgmt_divQuerySend(qName, isAnswer) {
    request.send();
 }
 
+/**
+ * Backend response handler for `${refUrlAPI}?cmd=query&type=qanswer|qtest`. This is returning
+ * the <div> element containing the query content - either the test or the answer versions.
+ */
 function actTestMgmt_onDivQueryResponse() {
    var jsonResponse = JSON.parse(this.response);
    const html = (this.status == 200) ? jsonResponse._qDiv : jsonResponse._error;
