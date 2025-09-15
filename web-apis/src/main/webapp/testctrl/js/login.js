af3fea7a1bdb36896d6f0820c7b65f81dd6fecdf
Florin.Teo <Florin.Teo@live.com>
2025-09-14 23:45:37 -0700

    Fix sid in adminPanel

--- web-apis/src/main/webapp/testctrl/js/login.js
+++ web-apis/src/main/webapp/testctrl/js/login.js
@@ -8,7 +8,8 @@ const btnLogin = document.getElementById("btnLogin");
 const txtOutput = document.getElementById("txtOutput");
 
 const urlAPI = window.location.origin + "/web-apis/testctrl";
-const urlAdminPanelJSP = window.location.origin + "/web-apis/testctrl/adminPanel.jsp?ver=1.1";
+const urlAdminPanelJSP = window.location.origin + "/web-apis/testctrl/adminPanel.jsp";
+const urlAdminPanelVer = "1.1";
 
 // #region: event listeners
 document.addEventListener("DOMContentLoaded", onPageLoad);
@@ -42,7 +43,7 @@ function onLoginResponse() {
     var jsonResponse = JSON.parse(this.response);
     if (this.status == 200) {
         // when successful or user already logged in, redirect to the AdminPanel page
-        window.location.href = `${urlAdminPanelJSP}?sid=${jsonResponse._sid}&name=${edtName.value}&ver=1.1`;
+        window.location.href = `${urlAdminPanelJSP}?sid=${jsonResponse._sid}&name=${edtName.value}&ver=${urlAdminPanelVer}`;
     } else {
         // otherwise display the response on the login page.
         txtOutput.innerHTML = `[${this.status}] ${jsonResponse._error}`;
