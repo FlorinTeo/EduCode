d6b602f158a049e40ce69ef251dc43ec2f5069c3
Florin.Teo <Florin.Teo@live.com>
2026-09-04 19:43:26 -0700

    Refactor UHeader constructor for improved clarity and null handling

--- web-apis/src/main/java/testctrl/testmgmt/UHeader.java
+++ web-apis/src/main/java/testctrl/testmgmt/UHeader.java
@@ -1,17 +1,15 @@
 package testctrl.testmgmt;
 
 public class UHeader {
-    public String username;
-    public String first_name;
-    public String last_name;
-    public String roles;
-    public String tags;
+    public String display_name;
+    public String[] roles;
+    public String[] tags;
     
-    public UHeader(String username, String first_name, String last_name, String roles, String tags) {
-        this.username = username;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.roles = roles;
-        this.tags = tags;
+    public UHeader(String first_name, String last_name, String aka_name, String roles, String tags) {
+        display_name = (aka_name == null || aka_name.isEmpty())
+            ? aka_name
+            : String.format("%s %s", first_name, last_name);
+        this.roles = roles.split("\\|");
+        this.tags = (tags != null) ?  tags.split("\\|") : new String[0];
     }
 }
